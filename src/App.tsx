6950177137ac0a33343e3076223de907fde87a5b
BryceeL <bcllamc280@gmail.com>
2025-04-30 15:17:33 -0700

    Reorganized pages in the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css'
 
 //Pages
-import ResultPage from './pages/InputPage/ResultPage';
+import ResultPage from './pages/ResultsPage/ResultPage';
 import InputPage from './pages/InputPage/InputPage';
 
 function App() {
