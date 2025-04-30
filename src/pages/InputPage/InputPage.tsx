d2c4f6531521af57cc968a28a69d1773b57a39fa
BryceeL <bcllamc280@gmail.com>
2025-04-30 15:07:03 -0700

    Established framework/foundation
    -Rid all of the template's css
    -Organized the project
    -Initalized routes and their respective pages

---
+++ src/pages/InputPage/InputPage.tsx
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+function InputPage() {
+    return (
+        <div>
+            Input Page
+        </div>
+    )
+}
+
+export default InputPage;
---
+++ src/pages/InputPage/ResultPage.tsx
@@ -0,0 +1,12 @@
+/*TODO: Retrieve from local storage the neccessary information
+to query edjoin jobpostings*/
+
+function ResultPage() {
+    return (
+        <div>
+            Result Page
+        </div>
+    )
+}
+
+export default ResultPage;
