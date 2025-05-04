b2701b69539e4ef83631c02aac87833a42752b84
BryceeL <bcllamc280@gmail.com>
2025-05-03 18:36:37 -0700

    attempted scraping
    issue: none of the pages load

--- src/pages/ResultsPage/ResultPage.tsx
+++ src/pages/ResultsPage/ResultPage.tsx
@@ -1,6 +1,23 @@
 /*TODO: Retrieve from local storage the neccessary information
 to query edjoin jobpostings*/
 
+import puppeteer from 'puppeteer'
+
+const getData = async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+    })
+
+    const page = await browser.newPage()
+
+    await page.goto("http://quotes.toscrape.com/", {
+        waitUntil: "domcontentloaded",
+    })
+}
+
+// getData()
+
 function ResultPage() {
     return (
         <div>
