2494e15e2f7026f779dac64665d0a34e77e71e5e
BryceeL <bcllamc280@gmail.com>
2025-05-10 19:57:48 -0700

    Updated to pass parameters through API

--- src/pages/ResultsPage/ResultPage.tsx
+++ src/pages/ResultsPage/ResultPage.tsx
@@ -1,27 +1,28 @@
 /*TODO: Retrieve from local storage the neccessary information
 to query edjoin jobpostings*/
-
-import puppeteer from 'puppeteer'
-
-const getData = async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: null,
-    })
-
-    const page = await browser.newPage()
-
-    await page.goto("http://quotes.toscrape.com/", {
-        waitUntil: "domcontentloaded",
-    })
-}
-
-// getData()
+import { useState, useEffect, useRef } from 'react';
 
 function ResultPage() {
+    const [data, setData]: any = useState("");
+
+    const calledRef = useRef(false);
+    useEffect(() => {
+        if (calledRef.current) return;
+        calledRef.current = true;
+
+        console.log('Calling API...');
+        fetch(`http://localhost:3001/api/scrape_jobs?keydata=${encodeURIComponent("tee")}`)
+          .then(res => res.json())
+          .then(data => setData(data))
+          .catch(err => console.error('Fetch error:', err));
+    }, []);
+
+    console.log(data)
+    
     return (
         <div>
             Result Page
+            
         </div>
     )
 }
