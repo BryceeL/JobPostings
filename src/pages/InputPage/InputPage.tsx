76a2d9caece97e0065cc3dbd27c079feb546e21f
BryceeL <bcllamc280@gmail.com>
2025-05-10 19:57:21 -0700

    Makes use of the Feed component
    The page no longer contains the state for each feed and respective input. Those states are moved to the Feed component.

--- src/pages/InputPage/InputPage.tsx
+++ src/pages/InputPage/InputPage.tsx
@@ -1,35 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import Feed from '../../components/Feed/Feed';
 
 function InputPage() {
-    //local storage states
-    const [distrctList, setDistrictList]: any = useState([])
-    const [keywordsList, setKeyWordsList] : any = useState([])
-
-    //input states
-    const [groupInput, setGroupInput] : any = useState("")
-    const [keywordInput, setKeywordInput] : any = useState("")
-
-    function getGroupInput() {
-        console.log(groupInput);
-    }
-
+    
     return (
-       
-        <div>
-            Input Page
-            <div className='group-feed'>
-
-            </div>
-            <input
-                type="text"
-                id="group"
-                value={groupInput}
-                placeholder="Input Group Name"
-                className=''
-                onChange={(e) => setGroupInput(e.target.value)}
-            />
-            <button onClick={() => getGroupInput()}>See input</button>
+        <div className='input-page'>            
+            <Feed
+                keyName={"districtKey"}
+            ></Feed>
         </div>
     )
 }
