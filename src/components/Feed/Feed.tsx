098609068af373e61f53b1ef6f12ecb2d68f1c5a
BryceeL <bcllamc280@gmail.com>
2025-05-10 19:55:54 -0700

    Added Feed
    -Maintains its own states
    -Retrieves and saves list data to storage
    -A feed to display all entries of the list

---
+++ src/components/Feed/Feed.tsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react';
+import FeedItem from "../Item/FeedItem";
+
+import "./Feed.css"
+
+type feedTypes = {
+    keyName: string
+}
+
+function Feed(props: feedTypes) {
+    const { keyName } = props
+
+    const [list, setList]: any = useState([])
+    const [input, setInput]: any = useState("")
+
+    const value = JSON.parse(localStorage.getItem(keyName) || '""')
+
+    useEffect(() => {
+        if (value != "") {
+            setList(value)
+        }
+
+    }, []);
+
+    //update list to localstorage and clear input box
+    function addToList() {
+        setList([...list, input])
+        localStorage.setItem(keyName, JSON.stringify([...list, input]))
+        setInput("")
+    }
+
+    //return items from the list except 'deleteItem'
+    function deleteFromList(deleteItem: string) {
+        const newList: any = list.filter((item: string) => {
+            return item != deleteItem
+        })
+        setList(newList)
+        localStorage.setItem(keyName, JSON.stringify(newList))
+    }
+
+    return (
+        <div className='feed'>
+            <input
+                type='text'
+                id='group'
+                value={input}
+                placeholder={`Input District's Name`}
+                onChange={(e) => setInput(e.target.value)}
+            />
+
+            <button onClick={() => addToList()}>Add</button>
+
+            <div className="feed-container">
+                {list.map((item: any) => (
+                    <FeedItem
+                        item={item}
+                        deleteFunction={deleteFromList}
+                    ></FeedItem>
+                ))}
+            </div>
+        </div>
+
+    )
+}
+
+export default Feed;
