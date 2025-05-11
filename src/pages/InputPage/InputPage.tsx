1ce62775e0752da0885db343284e8abea5406999
BryceeL <bcllamc280@gmail.com>
2025-05-10 20:13:32 -0700

    Updated InputPage
    -Added flexbox
    -Add another feed for keywords
    -Add button to navigate to result page

--- src/pages/InputPage/InputPage.tsx
+++ src/pages/InputPage/InputPage.tsx
@@ -1,12 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
 import Feed from '../../components/Feed/Feed';
 
+import "./InputPage.css"
+
 function InputPage() {
-    
+    const navigate = useNavigate();
     return (
         <div className='input-page'>            
             <Feed
-                keyName={"districtKey"}
+                keyName={"districts"}
+                placeHolderText={`Input a district's Name`}
             ></Feed>
+
+            <Feed
+                keyName={"keywords"}
+                placeHolderText={`Input a keyword`}
+            ></Feed>
+            <button onClick={() => navigate("/results")}>Results</button>
         </div>
     )
 }
