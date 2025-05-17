c403f69208f8227c15463d16788dab3982000ceb
BryceeL <bcllamc280@gmail.com>
2025-05-17 10:34:17 -0700

    Update ResultPage.tsx
    -Makes use of lists localstorage
    -Iterates each district and passes keywords to get request

--- src/pages/ResultsPage/ResultPage.tsx
+++ src/pages/ResultsPage/ResultPage.tsx
@@ -3,26 +3,40 @@ to query edjoin jobpostings*/
 import { useState, useEffect, useRef } from 'react';
 
 function ResultPage() {
-    const [data, setData]: any = useState("");
+    const [data, setData]: any = useState("")
+    const [scraped, setScraped]: any = useState(0)
+
+    const districtsList = JSON.parse(localStorage.getItem("districts") || '""')
+    const keywordsList = JSON.parse(localStorage.getItem("keywords") || '""')
 
     const calledRef = useRef(false);
     useEffect(() => {
         if (calledRef.current) return;
         calledRef.current = true;
 
-        console.log('Calling API...');
-        fetch(`http://localhost:3001/api/scrape_jobs?keydata=${encodeURIComponent("tee")}`)
-          .then(res => res.json())
-          .then(data => setData(data))
-          .catch(err => console.error('Fetch error:', err));
+        console.log(keywordsList)
+
+        districtsList.forEach((district: string) => {
+            console.log(`Calling Scraping API for:${district}`)
+            fetch(`http://localhost:3001/api/scrape_jobs?district=${encodeURIComponent(district)}&keywords=${encodeURIComponent(keywordsList.join(','))}`)
+                .then(res => res.json())
+                .then(data => {
+                    setData(data)
+                    console.log(data)
+                    
+                    setScraped(scraped + 1)
+                } )
+                .catch(err => console.error('Fetch error:', err))
+            
+        });
     }, []);
 
-    console.log(data)
+    
     
     return (
         <div>
             Result Page
-            
+            {scraped}
         </div>
     )
 }
