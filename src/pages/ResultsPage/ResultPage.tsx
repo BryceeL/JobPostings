f46c0e8b457ed793364cca88568416b22c673095
BryceeL <bcllamc280@gmail.com>
2025-05-17 16:42:04 -0700

    Update ResultPage.tsx
    -Counts the amount districts scraped
    -Lists out the jobs and links from the response
    -Temporary template blob for textfile download

--- src/pages/ResultsPage/ResultPage.tsx
+++ src/pages/ResultsPage/ResultPage.tsx
@@ -1,42 +1,73 @@
-/*TODO: Retrieve from local storage the neccessary information
-to query edjoin jobpostings*/
 import { useState, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+type JobTypes = {
+    jobTitle: string,
+    jobLink: string
+}
 
 function ResultPage() {
-    const [data, setData]: any = useState("")
-    const [scraped, setScraped]: any = useState(0)
+    const [jobPostings, setJobPostings] = useState<JobTypes[]>([])
+    const [scrapeAmount, setScrapeAmount] = useState(0)
+    const [scrapeTarget, setScrapeTarget] = useState("")
+
+    const [finished, setFinished]: any = useState(false)
 
     const districtsList = JSON.parse(localStorage.getItem("districts") || '""')
     const keywordsList = JSON.parse(localStorage.getItem("keywords") || '""')
 
+    const navigate = useNavigate();
     const calledRef = useRef(false);
+
+    const textContent = "a test"
+    const blob = new Blob([textContent], {type: 'text/plain'})
+    const url = URL.createObjectURL(blob)
+    
     useEffect(() => {
         if (calledRef.current) return;
         calledRef.current = true;
 
-        console.log(keywordsList)
-
-        districtsList.forEach((district: string) => {
-            console.log(`Calling Scraping API for:${district}`)
+        districtsList.forEach((district: string, i: number) => {
+            setScrapeTarget(district)
             fetch(`http://localhost:3001/api/scrape_jobs?district=${encodeURIComponent(district)}&keywords=${encodeURIComponent(keywordsList.join(','))}`)
-                .then(res => res.json())
+                .then(res => {
+                     if (!res.ok) throw new Error('Failed to fetch');
+                    return res.json();
+                })
                 .then(data => {
-                    setData(data)
-                    console.log(data)
-                    
-                    setScraped(scraped + 1)
+                    setJobPostings(data.matchingJobs)
+                    setScrapeAmount(scrapeAmount + 1)
+
+                    //Finished iterating
+                    if (i+1 == districtsList.length) {
+                        setScrapeTarget("")
+                        setFinished(true)
+                    }
                 } )
                 .catch(err => console.error('Fetch error:', err))
-            
-        });
-    }, []);
+        })
 
-    
-    
+      
+    }, [])
+
     return (
         <div>
-            Result Page
-            {scraped}
+            <p>Districts scraped: {scrapeAmount}/{districtsList.length}</p>
+            <button onClick={() => navigate("/")}>Back</button>
+
+            <p>{scrapeTarget ? `Scraping: '${scrapeTarget}'`: ""}</p>
+            {finished && (
+                jobPostings.length > 0 ? (jobPostings.map((jobPosting : JobTypes) => (
+                    <div>
+                        <a href={jobPosting.jobLink}>{jobPosting.jobTitle}</a>
+                        {/* <button onClick={() => { window.location.href = jobPosting.jobLink}}>{jobPosting.jobTitle}</button> */}
+                    </div>
+                ))) : "No matching job posts found"
+            )}
+
+            {finished && (
+                 <a href={url} download="jobPostings.txt">Download Job Postings</a>
+            )}
         </div>
     )
 }
