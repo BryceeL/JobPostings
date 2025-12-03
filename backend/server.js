98631d307477b1c001de6c02c4fee4270eb38290
BryceeL <bcllamc280@gmail.com>
2025-12-02 20:27:07 -0800

    install puppet configs

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 
 const routes = require('./routes')
 
+process.env.PUPPETEER_CACHE_DIR = process.env.PUPPETEER_CACHE_DIR || "/tmp/puppeteer";
+
 const app = express()
 app.use(cors ({
   origin: process.env.CLIENT_URL || "http://localhost:3001",
