2205ccc632ad28eb74a2ff0070b0eb14d33cfabd
BryceeL <bcllamc280@gmail.com>
2025-12-03 21:12:57 -0800

    Revert "Console log message correction"
    This reverts commit e6f9fef14adc15a36f2e7ab9c17d1de40e20f5ae.

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,5 @@ const port = process.env.PORT || 3001;
 app.use('/api', routes)
 
 app.listen(port, () => {
-  console.log(`Backend server running on ${port}`);
+  console.log(`Backend server running at http://localhost:${port}`);
 });
