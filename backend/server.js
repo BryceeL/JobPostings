bccf8e345abb94cfd5481048c4bbb7f51f744381
BryceeL <bcllamc280@gmail.com>
2025-12-13 21:31:43 -0800

    Fixed issues with puppeteer-core and @sparticuz

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,6 @@ import cors from "cors"
 
 import routes from "./routes.js"
 
-process.env.PUPPETEER_CACHE_DIR = process.env.PUPPETEER_CACHE_DIR || "/tmp/puppeteer";
-
 const app = express()
 app.use(cors ({
   origin: process.env.CLIENT_URL || "http://localhost:3001",
