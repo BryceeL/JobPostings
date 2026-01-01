3d97a10ea8db454574d5db07c04660e89666b7ed
BryceeL <bcllamc280@gmail.com>
2025-12-31 16:04:51 -0800

    Updated server.js for frontend compatiblity

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from "express"
 import cors from "cors"
-import routes from "./routes"
+import routes from "./routes.js"
 
 const app = express()
-app.use(cors())
+//Remove origin for development purposes
+app.use(cors({
+  origin: "https://blitzjobpostings.netlify.app/"
+}))
 
 const port = 3001
 
 app.use('/api', routes)
 
 app.listen(port, () => {
-  console.log(`Backend server running at http://localhost:${port}`);
+  console.log(`Backend server running`);
 });
