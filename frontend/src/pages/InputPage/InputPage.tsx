24c7b4803b147b531c83ff8aa5f92740cb2c9b4f
BryceeL <bcllamc280@gmail.com>
2026-02-02 18:57:10 -0800

    Update InputPage.tsx
    -Changed different keynames
    -Creates a profile state with lists of districts, keywords, and the different web elements
    -Implements a broken feed that utilize profile state

--- frontend/src/pages/InputPage/InputPage.tsx
+++ frontend/src/pages/InputPage/InputPage.tsx
@@ -1,43 +1,69 @@
 import { useNavigate } from 'react-router-dom';
 
+import ProfileFeed from '../../components/Feed/ProfileFeed';
 import Feed from '../../components/Feed/Feed';
 import WebElementInput from '../../components/WebElementInput/WebElementInput';
 
 import "./InputPage.css"
 
+import { useProfileState } from "../../hooks/ProfileState"
+
 function InputPage() {
+    const profileState = useProfileState({
+       districts : JSON.parse(localStorage.getItem("districts") || '[]'),
+       keywords : JSON.parse(localStorage.getItem("keywords") || '[]'),
+
+       institutionTitleContainerName: JSON.parse(localStorage.getItem("institutionTitleContainerName") || ''),
+       institutionTitleElementName: JSON.parse(localStorage.getItem("institutionTitleElementName") || ''),
+       jobContainerName: JSON.parse(localStorage.getItem("jobContainerName") || ''),
+       jobTitleName: JSON.parse(localStorage.getItem("jobTitleName") || ''),
+       webDomain: JSON.parse(localStorage.getItem("webDomain") || ''),
+
+       currentProfile: 0,
+       profileList: [],
+    })
+
     const navigate = useNavigate();
     return (
         <div className='input-page'>  
             <div className="inputs-container">
+                <ProfileFeed
+                    keyName={"currentProfile"}
+                    placeHolderText={'Input a scrape profile'}
+                ></ProfileFeed>
                 <Feed
-                    keyName={"districts"}
-                    placeHolderText={`Input a district's name`}
+                    feedState={profileState("districts")}
+                    placeHolderText={"Input a instituion's name"}
                 ></Feed>
-                <Feed
+                {/* <Feed
                     keyName={"keywords"}
                     placeHolderText={`Input a keyword`}
-                ></Feed>
+                ></Feed> */}
                 <div className='web-inputs-container'>
+                    <WebElementInput
+                        text={"Web Domain"}
+                        placeHolderText={'https://www.edjoin.org'}
+                        keyName={"webDomain"}
+                    ></WebElementInput>
                     <WebElementInput
                         text={"Job Container Name"}
                         placeHolderText={'.job-contain'}
-                        keyName={"Job Container Name"}
+                        keyName={"jobContainerName"}
                     ></WebElementInput>
                     <WebElementInput
                         text={"Job Title Name"}
                         placeHolderText={'.card-job-title'}
-                        keyName={"Job Title Name"}
+                        keyName={"jobTitleName"}
                     ></WebElementInput>
                     <WebElementInput
-                        text={"Organization Title Container Name"}
+                        text={"Institution Title Container Name"}
                         placeHolderText={'.bioBox'}
-                        keyName={"Organization Title Container Name"}
+                        keyName={"institutionTitleContainerName"}
                     ></WebElementInput>
                     <WebElementInput
                         text={"Organization Title Element Name"}
                         placeHolderText={'h1'}
-                        keyName={"Organization Title Element Name"}
+                        keyName={"institutionTitleElementName"}
                     ></WebElementInput>
                 </div>
               
